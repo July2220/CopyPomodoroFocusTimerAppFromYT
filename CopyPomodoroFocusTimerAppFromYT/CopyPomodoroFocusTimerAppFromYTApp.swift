4b1822c229c0d9659e47274245820b3a0628a5c3
July Tao <tjjsilwolf@gmail>
2023-04-25 11:36:10 +0800

    Initial Commit

---
+++ CopyPomodoroFocusTimerAppFromYT/CopyPomodoroFocusTimerAppFromYTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyPomodoroFocusTimerAppFromYTApp.swift
+//  CopyPomodoroFocusTimerAppFromYT
+//
+//  Created by july on 2023/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CopyPomodoroFocusTimerAppFromYTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
